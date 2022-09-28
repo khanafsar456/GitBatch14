8fc77456fb7d21602deefd1faea48a3cc8f1a804
khanafsar456 <khanafsar456@gmail.com>
2022-09-27 18:11:29 -0700

    third commet

--- src/class03B14/Class03.java
+++ src/class03B14/Class03.java
@@ -5,6 +5,7 @@ public class Class03 {
 	public static void main(String[] args) {
 	System.out.println("hey this is a git class");
 	System.out.println("hey this is a change in my project");
+	System.out.println("how are you this a new version");
 
 	}
 
